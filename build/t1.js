850f8e4da88451bb115d0fb0c738de89689ad9f8
david <davidu@metropolia.fi>
2026-09-07 12:03:45 +0300

    refactor: update package dependencies and remove unused greeting function
    - Updated rollup-watch from version 4.3.1 to 3.2.2
    - Updated tsc-watch from version 6.2.0 to 7.2.1
    - Removed unused greeting function from task1/src/greeting.ts
    - Enhanced t1.ts to implement a shopping cart feature with item input and total cost calculation
    - Added rootDir configuration in tsconfig.json for better project structure
    - Generated new build files for greeting.js and t1.js

--- build/t1.js
+++ build/t1.js
@@ -1,15 +1,6 @@
-// TODO: Write type (or interface) Item
-// TODO: Define the properties 'name', 'price', and 'quantity' within the type
-type Item ={
-    name: string;
-    price: number;
-    quantity: number;
-}
-
-
+"use strict";
 // Create an empty array named 'cart' to store the items
-const cart: Item[] = [];
-
+const cart = [];
 // TODO: Implement a loop to prompt the user for item details
 // Use 'while' loop to keep prompting until an empty item name is entered
 while (true) {
@@ -17,28 +8,22 @@ while (true) {
     const itemName = prompt('Enter item name:');
     const itemPrice = prompt('Enter item price:');
     const itemQuantity = prompt('Enter item quantity:');
-
     // Break the loop if an empty item name is entered
-     if (!itemName || !itemPrice || !itemQuantity) {
+    if (!itemName || !itemPrice || !itemQuantity) {
         break;
     }
-
-    if(isNaN(Number(itemPrice)) || isNaN(Number(itemQuantity))) {
+    if (isNaN(Number(itemPrice)) || isNaN(Number(itemQuantity))) {
         break;
     }
-
-
     // Create an item object and add it to the 'cart' array
-    const newItem: Item = {
+    const newItem = {
         name: itemName,
         price: Number(itemPrice),
         quantity: Number(itemQuantity)
     };
     cart.push(newItem);
 }
-
 // Calculate the total cost using the 'map' and 'reduce' functions
 const totalCost = cart.map(item => item.price * item.quantity).reduce((sum, cost) => sum + cost, 0);
-
 // Display the total cost to the user
 console.log(`Total cost of the shopping cart: $${totalCost.toFixed(2)}`);
