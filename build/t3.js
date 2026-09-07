bbd8e4df5ce9ef0ea977001352439e2b52a92027
david <davidu@metropolia.fi>
2026-09-07 17:28:36 +0300

    feat: implement squareRoot and promptForBook functions, update tsconfig, and add HTML files

---
+++ build/t3.js
@@ -0,0 +1,22 @@
+// TODO: Implement the promptForBook function
+function promptForBook() {
+    // TODO: Prompt user for book details (title, author, publication year)
+    const bookTitle = prompt("Enter book title") ?? "" /* TODO: Get user input for title */;
+    const bookAuthor = prompt("Enter input for author") ?? "" /* TODO: Get user input for author */;
+    const bookPublicationYear = Number(prompt("Enter publication year")) /* TODO: Get user input for publication year */;
+    // TODO: Create an object of type 'Book' with the entered values
+    const book = {
+        title: bookTitle,
+        author: bookAuthor,
+        publicationYear: bookPublicationYear,
+    };
+    return book;
+}
+// TODO: Call the promptForBook function to get the book details
+const bookDetails = promptForBook();
+// TODO: Display the details of the book object to the user
+console.log("Book Details:");
+console.log(`Title: ${bookDetails.title}`);
+console.log(`Author: ${bookDetails.author}`);
+console.log(`Publication Year: ${bookDetails.publicationYear}`);
+export {};
